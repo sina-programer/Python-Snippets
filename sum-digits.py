7ca5587627ace4a52b434b1e97dc8a3eb407aadd
Sina.F <sina.programerr@gmail.com>
2024-04-22 02:50:49 +0330

    Update `sum-digits`

--- sum-digits.py
+++ sum-digits.py
@@ -14,13 +14,22 @@ def sum_digits_str(number):
 
     return result
 
+
 def sum_digits_str_advanced(number):
     return sum(map(int, str(number)))
 
 
+def sum_digits_str_recursive(number):
+	summation = sum_digits_str_advanced(number)
+	while summation != (_sum := sum_digits_str_advanced(summation)):
+		summation = _sum
+	return summation
+
+
 if __name__ == "__main__":
-    number = 123
-    print('Number: ', number)
-    print('Numerical Sum: ', sum_digits_numerical(number))
-    print('String Sum: ', sum_digits_str(number))
-    print('Advanced String Sum: ', sum_digits_str_advanced(number))
+    number = 1235
+    print('Number:', number)
+    print('Numerical Sum:', sum_digits_numerical(number))
+    print('String Sum:', sum_digits_str(number))
+    print('Advanced String Sum:', sum_digits_str_advanced(number))
+    print('Recursive String Sum:', sum_digits_str_recursive(number))
