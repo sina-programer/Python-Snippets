d35c6e18dfefcc58fd27234957aa671b924db361
Sina.F <sina.programerr@gmail.com>
2024-04-22 03:40:17 +0330

    Update `max-subarray`

--- max-subarray.py
+++ max-subarray.py
@@ -1,27 +1,20 @@
-import time
+def max_crossing(arr, start, stop, reverse=False):
+	d = -1 if reverse else +1
+	summation = float('-inf')
+	maximum = 0
+	_sum = 0  # increasing value for cummulative sum
+	for idx in range(start, stop+d, d):
+		_sum += arr[idx]
+		if _sum > summation:
+			summation = _sum
+			maximum = idx
+	return summation, maximum
 
-
-def max_crossing_subarray(arr, low, mid, high):
-    sum_left = float('-inf')
-    max_left = 0
-    summation = 0
-    for i in range(mid, low-1, -1):
-        summation += arr[i]
-        if summation > sum_left:
-            sum_left = summation
-            max_left = i
-
-    sum_right = float('-inf')
-    max_right = 0
-    summation = 0
-    for j in range(mid+1, high+1):
-        summation += arr[j]
-        if summation > sum_right:
-            sum_right = summation
-            max_right = j
-
-    return max_left, max_right, sum_left+sum_right
-
+def max_crossing_subarray(arr, low, high):
+	mid = (low + high) // 2
+	sum_left, max_left = max_crossing(arr, mid, low, reverse=True)
+	sum_right, max_right = max_crossing(arr, mid+1, high)
+	return max_left, max_right, sum_left+sum_right
 
 def max_subarray(arr, low, high):
     if low == high:
@@ -30,7 +23,7 @@ def max_subarray(arr, low, high):
     mid = (low + high) // 2
     left_low, left_high, left_sum = max_subarray(arr, low, mid)
     right_low, right_high, right_sum = max_subarray(arr, mid+1, high)
-    cross_low, cross_high, cross_sum = max_crossing_subarray(arr, low, mid, high)
+    cross_low, cross_high, cross_sum = max_crossing_subarray(arr, low, high)
 
     if left_sum >= right_sum and left_sum >= cross_sum:
         return left_low, left_high, left_sum
@@ -38,44 +31,37 @@ def max_subarray(arr, low, high):
         return right_low, right_high, right_sum
     return cross_low, cross_high, cross_sum
 
-
 def division(arr):
     low, high, maximum_sum = max_subarray(arr, 0, len(arr)-1)
     return arr[low:high+1]
 
 
-def brute_force(array):
-    maximum = array[0]
+def brute_force(numbers):
+    maximum = numbers[0]
     right = 1
     left = 1
 
-    for i in range(len(array)):
+    for i in range(len(numbers)):
         current_sum = 0
-        for j in range(i, len(array)):
-            current_sum += array[j]
+        for j in range(i, len(numbers)):
+            current_sum += numbers[j]
             if current_sum > maximum:
                 maximum = current_sum
                 left = i
                 right = j
 
-    return array[left:right+1]
+    return numbers[left:right+1]
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     numbers = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7]
-    print('Numbers:', numbers, '\n')
+    print('Numbers:', numbers)
 
-    start_time = time.perf_counter()
     brute_array = brute_force(numbers)
     print(f"Brute-Force Method \nsum: {sum(brute_array)}")
     print(brute_array)
-    stop_time = time.perf_counter()
-    print('running brute-force took', stop_time-start_time, 'seconds')
     print()
 
-    start_time = time.perf_counter()
     division_array = division(numbers)
     print(f"Division Method \nsum: {sum(division_array)}")
     print(division_array)
-    stop_time = time.perf_counter()
-    print('running division took', stop_time-start_time, 'seconds')
